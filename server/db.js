4c70d2eefaa8b62192ee0aed4df5fea1a60e5948
dtoney12 <dtoney12@gmail.com>
2017-04-02 09:56:04 -0700

    back to pool

--- server/db.js
+++ server/db.js
@@ -1,26 +1,19 @@
 var mysql = require('mysql');
 var state = require('./state');
-var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
 
-// var pool = mysql.createPool({
-var connection = mysql.createConnection({
-  // host: 'localhost',
+var pool = mysql.createPool({
   host: 'us-cdbr-iron-east-03.cleardb.net',
-  // port: 3306,
-  // user: 'root',
-    user: 'b6688425c7d55d',
-  // password: '',
-    password: 'b6eb9da1',
-  // database: ''
-    database: 'heroku_5a562928f98c27c'
+  user: 'b6688425c7d55d',
+  password: 'b6eb9da1',
+  database: 'heroku_5a562928f98c27c'
 });
 
 module.exports = {
 	addName: function(enteredName, player, callback){
-	  	// connection.getConnection(function(err, connection) {
-	  	// 	if (err) {
-	  	// 		throw err;
-	  	// 	}
+	  	pool.getConnection(function(err, connection) {
+	  		if (err) {
+	  			throw err;
+	  		}
 	  		var queryString = `SELECT * from player WHERE name='${enteredName}';`;	
 	  		connection.query(queryString, function(err, playerInfo) {
 	  			if (err) {
@@ -35,6 +28,7 @@ module.exports = {
 				  			}
 				  			console.log('\nname added to db -----> :' + enteredName);
 				  			player.set( { update: `${enteredName} <--- Please log in` } );
+				  			connection.release();
 				  			return;
 				  		});
 				  	}
@@ -44,23 +38,25 @@ module.exports = {
 		  				console.log('removing name from model -----> :' + enteredName)
 		  				player.set( { update: `${enteredName} <--- already logged in` } );
 		  				player.set( { name: '' } );
+		  				connection.release();
 		  				return;
 		  			} else if (playerInfo[0].loggedin === 0) {
 		  				console.log('\nUser exists in db but not logged in -----> :' + enteredName);
 		  				console.log('Requesting user to log in -----> :' + enteredName)
 		  				player.set( { update: `${enteredName} <--- Please log in` } );
 		  				player.set( { name: enteredName } );
+		  				connection.release();
 		  				return;
 		  			}
 	  			}
-		  	});
-	  	// });
+		  	})
+	  	});
 	},
 	checkPassword: function(enteredPW, player, callback){
-	  	// connection.getConnection(function(err, connection) {
-	  	// 	if (err) {
-	  	// 		throw err;
-	  	// 	}
+	  	pool.getConnection(function(err, connection) {
+	  		if (err) {
+	  			throw err;
+	  		}
 	  		var queryString = `SELECT * from player WHERE name='${player.attributes.name}';`;	
 	  		connection.query(queryString, function(err, playerInfo) {
 	  			if (err) {
@@ -77,6 +73,7 @@ module.exports = {
 				  			player = state.registerNewPlayer(player.attributes.name);
 				  			state.logInPlayer(player, playerInfo[0]);
 				  			console.log('\nPassword \'' + enteredPW + '\' set and Registered New Player ------> Logging in player-----> :' + player.attributes.name);
+				  			connection.release();
 				  			return;
 				  		});
 	  				} else {
@@ -88,11 +85,13 @@ module.exports = {
 					  			}
 		  						state.logInPlayer(player, playerInfo[0]);
 		  						console.log('\nPassword \'' + enteredPW + '\' matches ------> Logging in player-----> :' + player.attributes.name);
+		  						connection.release();
 					  			return;
 					  		});
 	  					} else {
 	  						console.log('\nPassword \'' + enteredPW + '\' does not match for Player-----> :' + player.attributes.name);
 	  						state.updateClientStatus(player, 'Password incorrect or already logged in, Sorry');
+	  						connection.release();
 	  						return;
 	  					}
 	  				}
@@ -101,16 +100,17 @@ module.exports = {
 	  				console.log('Rejecting password update request -----> :' + enteredPW)
 	  				player.set( { update: `${player.attributes.name} <--- user doesn't exist` } );
 	  				player.set( { password: '' } );
+	  				connection.release();
 	  				return;
 	  			}
 		  	});
-	  	// });
+	  	});
 	},
 	logOutPlayer: function(player){
-	  	// connection.getConnection(function(err, connection) {
-	  	// 	if (err) {
-	  	// 		throw err;
-	  	// 	}
+	  	pool.getConnection(function(err, connection) {
+	  		if (err) {
+	  			throw err;
+	  		}
 	  		var queryString = `SELECT * from player WHERE name='${player.attributes.name}';`;	
 	  		connection.query(queryString, function(err, playerInfo) {
 	  			playerInfo = playerInfo[0];
@@ -127,18 +127,20 @@ module.exports = {
 				  			}
 				  			console.log(`\ndb loggedIn has been set to false for >>${player.attributes.name}<<`);
 				  			state.logOutPlayer(player);
+				  			connection.release();
 				  			return;
 				  		});
 		  			}
 	  			} else {
 	  				console.log('\nPlayer name does not exist in db -----> :' + player.attributes.name);
+	  				connection.release();
 	  				return;
 	  			}
 		  	})
-	  	// });
+	  	});
 	},
 	// getCashWait: function(player) {
-	// 	connection.getConnection(function(err, connection) {
+	// 	pool.getConnection(function(err, connection) {
 	//   		if (err) {
 	//   			throw err;
 	//   		}
@@ -168,10 +170,10 @@ module.exports = {
 	//   	});
 	// },	
 	getCash: function(player) {
-		// connection.getConnection(function(err, connection) {
-	 //  		if (err) {
-	 //  			throw err;
-	 //  		}
+		pool.getConnection(function(err, connection) {
+	  		if (err) {
+	  			throw err;
+	  		}
 	  		var queryString = `SELECT * from player WHERE name='${player.attributes.name}';`;	
 	  		connection.query(queryString, function(err, playerInfo) {
 	  			if (err) {
@@ -191,22 +193,22 @@ module.exports = {
 				  			if (playerInfo.length) {			  			
 					  			console.log(`\ndb accountCash has been set to 1000, getCashWait to 60 for >>${player.attributes.name}<<`);
 					  			state.syncPlayerStateDb(player, updatedPlayerInfo[0]);
+					  			connection.release();
 					  			return;
 					  		}
 					  	});
 					});
 	  			} else {
 	  				console.log('\nPlayer name does not exist in db -----> :' + player.attributes.name);
+	  				connection.release();
 	  				return;
 	  			}
 		  	})
-	  	// });
+	  	});
 	},	
 	initDbPlayer: function() {
-		// pool.getConnection(function(err, connection) {
-		connection.connect(function(err) {
+		pool.getConnection(function(err, connection) {
 	  		if (err) {
-	  			console.log(err)
 	  			throw err;
 	  		}
 	  		var queryString = `DROP TABLE IF EXISTS player;`;	
@@ -214,16 +216,16 @@ module.exports = {
 	  			if (err) {
 	  				throw err;
 	  			}
-	  		// 	var queryString = `CREATE DATABASE dtpoker;`;
-	  		// 	pool.query(queryString, function(err) {
-		  	// 		if (err) {
-		  	// 			throw err;
-		  	// 		}
-		  	// 		var queryString = `USE dtpoker;`;
-		  	// 		pool.query(queryString, function(err) {
-			  // 			if (err) {
-			  // 				throw err;
-			  // 			}
+	  			// var queryString = `CREATE DATABASE dtpoker;`;
+	  			// connection.query(queryString, function(err) {
+		  		// 	if (err) {
+		  		// 		throw err;
+		  		// 	}
+		  		// 	var queryString = `USE dtpoker;`;
+		  		// 	connection.query(queryString, function(err) {
+			  	// 		if (err) {
+			  	// 			throw err;
+			  	// 		}
 			  			var queryString = `CREATE TABLE player ( id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, \
 	 					name CHAR(140), password CHAR(140), loggedIn BOOLEAN, accountCash INT, getCashWait INT, UNIQUE (name) );`;
 		  				connection.query(queryString, function(err) {
@@ -235,15 +237,15 @@ module.exports = {
 					  			if (err) {
 					  				throw err;
 					  			}
-			  					// pool.release();
+			  					connection.release();
 			  					return;
 					  		});
 					  	return;
 				  		});
-				 //  	return;
-			  // 		});
-			  // 	return;
-			  // 	});
+				  // 	return;
+			  	// 	});
+			  	// return;
+			  	// });
 			  return;
 		  	});
 		return;
