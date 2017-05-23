a0b79fb3d0db3cf72c31e041d144a8471b2dc7e2
dtoney12 <dtoney12@gmail.com>
2017-05-22 17:39:52 -0700

    back to 80

--- public/session.js
+++ public/session.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", (e)=> { 
 	// const ws = new WebSocket(location.origin.replace(/^http/, 'ws'));
-  const ws = new WebSocket('wss://backdoorpoker-dev.us-west-1.elasticbeanstalk.com');
+  const ws = new WebSocket('ws://backdoorpoker-dev.us-west-1.elasticbeanstalk.com');
   // const ws = new WebSocket('ws://127.0.0.1');
   window.ws = ws;
   const wsSend = function(event, type, booleanValue) {
--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const Backbone = require('./non_config/backbone');
 const Promise = require('bluebird')
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 80;
 // const PORT = 443;
 const CLIENT_FILES = path.join(__dirname, '/../public');
 const express = require('express');
