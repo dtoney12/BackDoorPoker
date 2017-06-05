a0494bce2da3acacecc80347adfcb3761ee9d0d8
dtoney12 <dtoney12@gmail.com>
2017-06-05 13:05:01 -0700

    qweqw

--- public/bundle.js
+++ public/bundle.js
@@ -10654,48 +10654,44 @@ var TableView = function (_React$Component) {
                 }
             }, 500);
             // Object.assign polyfill for IE 11
-            if (typeof Object.assign != 'function') {
-                Object.assign = function (target) {
-                    'use strict';
 
-                    if (target == null) {
-                        throw new TypeError('Cannot convert undefined or null to object');
-                    }
+            Object.assign = function (target) {
+                'use strict';
 
-                    target = Object(target);
-                    for (var index = 1; index < arguments.length; index++) {
-                        var source = arguments[index];
-                        if (source != null) {
-                            for (var key in source) {
-                                if (Object.prototype.hasOwnProperty.call(source, key)) {
-                                    target[key] = source[key];
-                                }
+                if (target == null) {
+                    throw new TypeError('Cannot convert undefined or null to object');
+                }
+
+                target = Object(target);
+                for (var index = 1; index < arguments.length; index++) {
+                    var source = arguments[index];
+                    if (source != null) {
+                        for (var key in source) {
+                            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                                target[key] = source[key];
                             }
                         }
                     }
-                    return target;
-                };
-            }
+                }
+                return target;
+            };
 
             var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
             var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
             var concat = Function.bind.call(Function.call, Array.prototype.concat);
             var keys = Reflect.ownKeys;
-            if (!Object.values) {
-                Object.values = function values(O) {
-                    return reduce(keys(O), function (v, k) {
-                        return concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []);
-                    }, []);
-                };
-            }
 
-            if (!Object.entries) {
-                Object.entries = function entries(O) {
-                    return reduce(keys(O), function (e, k) {
-                        return concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []);
-                    }, []);
-                };
-            }
+            Object.values = function values(O) {
+                return reduce(keys(O), function (v, k) {
+                    return concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []);
+                }, []);
+            };
+
+            Object.entries = function entries(O) {
+                return reduce(keys(O), function (e, k) {
+                    return concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []);
+                }, []);
+            };
         }
     }, {
         key: 'componentDidUpdate',
