064cfb95a0ea631b5673834677d5313b8615e1bd
dtoney12 <dtoney12@gmail.com>
2017-06-05 13:01:06 -0700

    qwewq

--- public/bundle.js
+++ public/bundle.js
@@ -10676,6 +10676,11 @@ var TableView = function (_React$Component) {
                     return target;
                 };
             }
+
+            var reduce = Function.bind.call(Function.call, Array.prototype.reduce);
+            var isEnumerable = Function.bind.call(Function.call, Object.prototype.propertyIsEnumerable);
+            var concat = Function.bind.call(Function.call, Array.prototype.concat);
+            var keys = Reflect.ownKeys;
             if (!Object.values) {
                 Object.values = function values(O) {
                     return reduce(keys(O), function (v, k) {
